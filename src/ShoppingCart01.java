eba22f3e2bbaf08587f2ebe578dfab88abafd2b0
jargi <jon.givens@cdpipelinedevshop.com>
2022-08-18 10:48:24 -0400

    finished this section.

---
+++ src/ShoppingCart01.java
@@ -0,0 +1,14 @@
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+        // Declare and initialize String variables.  Do not initialize message yet.
+        String custname = "Jerry";
+        String itemDesc = "Pantaloons";
+        String message;
+        
+        // Assign the message variable 
+        message = custname + " wants to buy " + itemDesc;
+        
+        // Print and run the code
+        System.out.println(message);
+    }
+}
---
+++ src/ShoppingCart02.java
@@ -0,0 +1,21 @@
+public class ShoppingCart02 {
+    public static void main(String[] args) {
+        String custName = "Alex";
+        String itemDesc = "Shirts";
+        String message = custName+" wants to purchase a "+itemDesc;
+        
+        // Declare and initialize numeric fields: price, tax, quantity.   
+        double price = 15.5;
+        double tax = 1.15;
+        int quantity = 3;
+        // Declare and assign a calculated totalPrice
+        double totalPrice = (price + tax) * quantity;
+        
+        // Modify message to include quantity 
+        message = custName + " wants to purchase " + quantity + " " + itemDesc;
+        System.out.println(message);
+        
+        // Print another message with the total cost
+        System.out.println("Total cost with tax is: $" + totalPrice);
+    }    
+}
